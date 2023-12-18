00c3f133c01fcaf1b7e4f308b5987e550d9d277f
Gaby Tomasiello <gaby.cisco@gmail.com>
2023-12-18 04:07:41 -0300

    Se crea modulos game (components, pages y routing)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameFormComponent } from './modules/game/components/game-form/game-form.component';
+import { EditGameComponent } from './modules/game/pages/edit-game/edit-game.component';
+import { CreateGameComponent } from './modules/game/pages/create-game/create-game.component';
+import { GameComponent } from './modules/game/pages/game/game.component';
+import { GameListComponent } from './modules/game/pages/game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameFormComponent,
+    EditGameComponent,
+    CreateGameComponent,
+    GameComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule,
